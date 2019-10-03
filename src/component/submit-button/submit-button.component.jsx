6b8349d916e995e735968ea6e7688e66f2c73400
crs2here <crs2here@gmail.com>
2019-10-02 20:30:50 -0700

    create basic submit button

---
+++ src/component/submit-button/submit-button.component.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { StyledButton } from "./submit-button.style";
+
+const SubmitButton = () => <StyledButton primary>Submit</StyledButton>;
+
+export default SubmitButton;
