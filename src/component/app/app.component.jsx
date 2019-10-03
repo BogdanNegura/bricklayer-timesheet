30c635031e6ac02d7bce6945f04dbd666220bfc2
crs2here <crs2here@gmail.com>
2019-10-02 20:30:58 -0700

    implement submit button

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react'
-import WeekDays from '../week-days'
-import TotalHours from '../total-hours'
-import SalaryCalculator from '../salary-calculator'
-import './app.component.css'
+import React, { useState } from "react";
+import WeekDays from "../week-days";
+import TotalHours from "../total-hours";
+import SalaryCalculator from "../salary-calculator";
+import SubmitButton from "../submit-button";
+import "./app.component.css";
 
 const TimeSheetApp = () => {
-    const [totalHours, setTotalHours] = useState(0) 
+  const [totalHours, setTotalHours] = useState(0);
 
-    return (
-        <div>
-            <h1>Timesheet App</h1>
-            <WeekDays sth={setTotalHours}/>
-            <TotalHours th={totalHours}/>
-            <SalaryCalculator toho={totalHours}/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Timesheet App</h1>
+      <WeekDays sth={setTotalHours} />
+      <TotalHours th={totalHours} />
+      <SalaryCalculator toho={totalHours} />
+      <SubmitButton />
+    </div>
+  );
+};
 
-export default TimeSheetApp
+export default TimeSheetApp;
